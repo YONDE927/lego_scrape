b9514b2a8a8404aeac4bf8bf2216026cb41e0d60
YONDE927 <svyuta1202@gmail.com>
2018-10-13 16:11:27 +0900

    repair

--- scrape/bin/activate.fish
+++ scrape/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/vagrant_data/lego_scrape/scrape"
+set -gx VIRTUAL_ENV "/mnt/c/Users/svyut/ubuntufiles/scraping/lego_scrape/scrape"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
