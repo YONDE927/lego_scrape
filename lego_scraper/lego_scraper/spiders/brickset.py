cc79dc81aeda7e700bf524a1086768c89b2f3157
YONDE927 <svyuta1202@gmail.com>
2018-10-15 09:07:12 +0000

    finished

--- lego_scraper/lego_scraper/spiders/brickset.py
+++ lego_scraper/lego_scraper/spiders/brickset.py
@@ -12,4 +12,18 @@ class BricksetSpider(scrapy.Spider):
             meta=brickset.css('div.meta')
             number=meta.css('h1 span::text').re_first(r'(.+):')
             name=brickset.css('div.highslide-caption h1::text').extract_first()
-            print(number,name)
+            yield{
+                'number':number,
+                'name':name,
+                'image':brickset.css('img::attr(src)').re_first(r'(.+)\?'),
+                'theme':meta.css('.tags a')[1].css('a::text').extract_first(),
+                'subtheme':meta.css('.tags a.subtheme::text').extract_first(),
+                'year':meta.css('a.year::text').extract_first(),
+                'rating':meta.css('.rating::attr(title)').extract_first(),
+                'owners':brickset.css('dl.admin dd').re_first('(\d+) own this set'),
+                'want_it':brickset.css('dl.admin dd').re_first('(\d+) want it')
+            }
+
+        next_url=response.css('li.next a::attr("href")').extract_first()
+        if next_url:
+            yield scrapy.Request(next_url)
